cad583a8c8ec8f2e1dfa0ba7ac4e0f5b3ff4bd6e
Michael Xue <michaelxy@gmail.com>
2021-02-05 11:09:46 -0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+if [ ""$(echo $BACKGROUND_COLOR | tr '[:lower:]' '[:upper:]') = 'RED' ]; then
+    echo -e "\e[31mERROR: Invalid Color:\e[0m" ${BACKGROUND_COLOR}
+    EXIT_CODE=1
+else
+  echo -e "\e[32mValidation Complete - No Known Errors\e[0m"
+fi
+exit ${EXIT_CODE}
